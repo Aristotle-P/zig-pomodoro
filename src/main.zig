dae5d37278ac9cca1f2421b84e9cd21f703c227e
= <aristotlepoultney@gmail.com>
2023-07-07 03:20:54 -0400

    basic timer and args functionality

--- src/main.zig
+++ src/main.zig
@@ -1,19 +1,34 @@
 const std = @import("std");
 
 pub fn main() !void {
-    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    defer arena.deinit();
+    var general_purpose_allocator = std.heap.GeneralPurposeAllocator(.{}){};
+    const gpa = general_purpose_allocator.allocator();
+    const args = try std.process.argsAlloc(gpa);
+    defer std.process.argsFree(gpa, args);
 
-    const allocator = arena.allocator();
-    const args = std.process.argsWithAllocator(allocator);
-    var l_flag = []u16;
-    var s_flag = []u16;
+    var l_flag: u8 = undefined;
+    var s_flag: u8 = undefined;
+
+    for (args, 0..) |arg, i| {
+        if (i == 1) {
+            l_flag = try std.fmt.parseInt(u8, arg, 10);
+        }
+        if (i == 2) {
+            s_flag = try std.fmt.parseInt(u8, arg, 10);
+        } else {
+            continue;
+        }
+    }
 
     var timer = try std.time.Timer.start();
-    comptime var endTime = 10;
-    for (0..endTime) |_| {
+    var startTime = timer.read();
+    std.debug.print("Start time is: {d}\n", .{startTime});
+    for (0..l_flag) |_| {
         std.time.sleep(100_000_000_0);
         var currentTime = timer.read();
-        std.debug.print("hello world at {d}\n", .{currentTime});
+        std.debug.print("Current time is: {d}\n", .{currentTime});
     }
+
+    const endTime = timer.read();
+    std.debug.print("End time is: {d}\n", .{endTime});
 }
